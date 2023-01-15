4a921297dd1f5571e8ccfcb2263e390636979b6a
Diego <finndiego@hotmail.com>
2023-01-15 15:27:41 -0300

    Fifith commit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function mudaTema() {
   document.body.classList.toggle("dark");
+  var mudaTema = document.getElementById("mudaTema");
+  if(mudaTema.innerHTML == "🌙"){
+    mudaTema.innerHTML = "☀️";
+  }else{
+    mudaTema.innerHTML = "🌙";
+  }
 }
